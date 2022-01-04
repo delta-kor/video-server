ffd34c937217c0d6fd9c0d01b4c1c1e4f9ea0ca0
delta-kor <deltakbot1@gmail.com>
2022-01-04 15:59:02 +0900

    Fix cors pipe

--- src/pipes/cors.pipe.ts
+++ src/pipes/cors.pipe.ts
@@ -4,6 +4,8 @@ class CorsPipe {
   public static use(application: Application): void {
     application.use((req: TypedRequest, res: TypedResponse, next: NextFunction) => {
       res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
       next();
     });
   }
