d66f871e37039e4d26b04047d79e8eb0d8ee1a31
delta-kor <deltakbot1@gmail.com>
2022-01-04 16:06:12 +0900

    Add uploader

--- uploader.js
+++ uploader.js
@@ -1,1 +1,18 @@
-function upload(url, data) {}
+function upload(base, key, data) {
+  const url = `${base}/video`;
+
+  const lines = data.trim().split('\n');
+  for (const line of lines) {
+    const chips = line.split('\t');
+    const category = chips.slice(0, 3);
+    const numberDate = chips[3];
+    const date = new Date(`20${numberDate.slice(0, 2)}. ${numberDate.slice(2, 4)}. ${numberDate.slice(4, 6)}.`);
+    const title = chips[4];
+    const description = chips[5];
+    const cdnId = chips[6];
+    const payload = { cdnId, title, description, date: date.getTime(), category, key };
+    fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
+}
