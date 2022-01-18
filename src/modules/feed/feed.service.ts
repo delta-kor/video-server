2c78d3353ae5a3034a71de633b6041e15e81f8d6
delta-kor <deltakbot1@gmail.com>
2022-01-18 15:54:09 +0900

    Add shuffling array

--- src/modules/feed/feed.service.ts
+++ src/modules/feed/feed.service.ts
@@ -85,6 +85,11 @@ class FeedService extends Service {
       }
     }
 
+    for (let i = result.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [result[i], result[j]] = [result[j], result[i]];
+    }
+
     return result;
   }
 }
