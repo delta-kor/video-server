372d007ff28413ce79cab623a230d0169f695ed1
delta-kor <deltakbot1@gmail.com>
2022-02-26 19:17:03 +0900

    Add video model sorting

--- src/modules/video/video.service.ts
+++ src/modules/video/video.service.ts
@@ -12,7 +12,7 @@ class VideoService extends Service {
   public readonly videos: Video[] = [];
 
   public async load(): Promise<void> {
-    const videos = await VideoModel.find();
+    const videos = await VideoModel.find().sort({ _id: 1, date: 1 });
     this.videos.push(...videos);
   }
 
