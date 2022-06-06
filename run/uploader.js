6c39ee3967d2d9ad21d21dd13ba7020444d23315
delta-kor <deltakbot1@gmail.com>
2022-06-06 18:16:02 +0900

    Update uploader

--- run/uploader.js
+++ run/uploader.js
@@ -4,13 +4,12 @@ function upload(base, key, data) {
   const lines = data.trim().split('\n');
   for (const line of lines) {
     const chips = line.split('\t');
-    const category = chips.slice(0, 3);
-    const numberDate = chips[3];
-    const date = new Date(`20${numberDate.slice(0, 2)}. ${numberDate.slice(2, 4)}. ${numberDate.slice(4, 6)}.`);
-    const title = chips[4];
-    const description = chips[5];
+    const title = chips[0];
+    const description = chips[1];
+    const category = chips.slice(2, 5);
+    const date = new Date(chips[5]);
     const cdnId = chips[6];
-    const payload = { cdnId, title, description, date: date.getTime(), category, key };
+    const payload = { cdnId, title, description, category, date: date.getTime(), options: ['category', 'vod'], key };
     fetch(url, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(payload) })
       .then(res => res.json())
       .then(data => console.log(data));
