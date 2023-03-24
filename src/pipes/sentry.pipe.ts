487a0b547f374734623b77812eb2bb60613fdc0b
delta-kor <contact@izflix.net>
2023-03-24 21:23:02 +0900

    Add sentry profiler

--- src/pipes/sentry.pipe.ts
+++ src/pipes/sentry.pipe.ts
@@ -2,6 +2,7 @@ import { Application } from 'express';
 import * as Sentry from '@sentry/node';
 import * as Tracing from '@sentry/tracing';
 import { RewriteFrames } from '@sentry/integrations';
+import { ProfilingIntegration } from '@sentry/profiling-node';
 
 class SentryPipe {
   public static use(application: Application): void {
@@ -16,13 +17,15 @@ class SentryPipe {
         new Sentry.Integrations.Http({ tracing: true }),
         new Tracing.Integrations.Express({ app: application }),
         new Tracing.Integrations.Mongo({ useMongoose: true }),
+        new ProfilingIntegration(),
       ],
       beforeSendTransaction: event => {
         const ignores = ['POST /video/:id/beacon', 'GET /thumbnail/:id', 'GET /log/beacon'];
         if (event.transaction && ignores.includes(event.transaction)) return null;
         return event;
       },
-      tracesSampleRate: 0.3,
+      tracesSampleRate: 0.1,
+      profilesSampleRate: 1,
     });
 
     application.use(Sentry.Handlers.requestHandler());
